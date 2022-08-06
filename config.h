e55a1eb0b56893a1fbb8d8abab8f5ecb03222518
Mathias Estrup <32955302+mestru17@users.noreply.github.com>
2022-08-06 18:19:46 +0200

    Add OneDark theme

--- config.h
+++ config.h
@@ -134,47 +134,50 @@ static const char *fonts[]               = { "FiraCode Nerd Font:size=11" };
 #endif // BAR_PANGO_PATCH
 static const char dmenufont[]            = "FiraCode Nerd Font:size=11";
 
-static char c000000[]                    = "#000000"; // placeholder value
+#include "themes/onedark.h"
 
-static char normfgcolor[]                = "#bbbbbb";
-static char normbgcolor[]                = "#222222";
-static char normbordercolor[]            = "#444444";
-static char normfloatcolor[]             = "#db8fd9";
-
-static char selfgcolor[]                 = "#eeeeee";
-static char selbgcolor[]                 = "#005577";
-static char selbordercolor[]             = "#005577";
-static char selfloatcolor[]              = "#005577";
-
-static char titlenormfgcolor[]           = "#bbbbbb";
-static char titlenormbgcolor[]           = "#222222";
-static char titlenormbordercolor[]       = "#444444";
-static char titlenormfloatcolor[]        = "#db8fd9";
-
-static char titleselfgcolor[]            = "#eeeeee";
-static char titleselbgcolor[]            = "#005577";
-static char titleselbordercolor[]        = "#005577";
-static char titleselfloatcolor[]         = "#005577";
-
-static char tagsnormfgcolor[]            = "#bbbbbb";
-static char tagsnormbgcolor[]            = "#222222";
-static char tagsnormbordercolor[]        = "#444444";
-static char tagsnormfloatcolor[]         = "#db8fd9";
-
-static char tagsselfgcolor[]             = "#eeeeee";
-static char tagsselbgcolor[]             = "#005577";
-static char tagsselbordercolor[]         = "#005577";
-static char tagsselfloatcolor[]          = "#005577";
-
-static char hidnormfgcolor[]             = "#005577";
-static char hidselfgcolor[]              = "#227799";
-static char hidnormbgcolor[]             = "#222222";
-static char hidselbgcolor[]              = "#222222";
-
-static char urgfgcolor[]                 = "#bbbbbb";
-static char urgbgcolor[]                 = "#222222";
-static char urgbordercolor[]             = "#ff0000";
-static char urgfloatcolor[]              = "#db8fd9";
+// TODO: Find a way to default to this if no theme is included. See statuscolors below also
+// static char c000000[]                    = "#000000"; // placeholder value
+// 
+// static char normfgcolor[]                = "#bbbbbb";
+// static char normbgcolor[]                = "#222222";
+// static char normbordercolor[]            = "#444444";
+// static char normfloatcolor[]             = "#db8fd9";
+// 
+// static char selfgcolor[]                 = "#eeeeee";
+// static char selbgcolor[]                 = "#005577";
+// static char selbordercolor[]             = "#005577";
+// static char selfloatcolor[]              = "#005577";
+// 
+// static char titlenormfgcolor[]           = "#bbbbbb";
+// static char titlenormbgcolor[]           = "#222222";
+// static char titlenormbordercolor[]       = "#444444";
+// static char titlenormfloatcolor[]        = "#db8fd9";
+// 
+// static char titleselfgcolor[]            = "#eeeeee";
+// static char titleselbgcolor[]            = "#005577";
+// static char titleselbordercolor[]        = "#005577";
+// static char titleselfloatcolor[]         = "#005577";
+// 
+// static char tagsnormfgcolor[]            = "#bbbbbb";
+// static char tagsnormbgcolor[]            = "#222222";
+// static char tagsnormbordercolor[]        = "#444444";
+// static char tagsnormfloatcolor[]         = "#db8fd9";
+// 
+// static char tagsselfgcolor[]             = "#eeeeee";
+// static char tagsselbgcolor[]             = "#005577";
+// static char tagsselbordercolor[]         = "#005577";
+// static char tagsselfloatcolor[]          = "#005577";
+// 
+// static char hidnormfgcolor[]             = "#005577";
+// static char hidselfgcolor[]              = "#227799";
+// static char hidnormbgcolor[]             = "#222222";
+// static char hidselbgcolor[]              = "#222222";
+// 
+// static char urgfgcolor[]                 = "#bbbbbb";
+// static char urgbgcolor[]                 = "#222222";
+// static char urgbordercolor[]             = "#ff0000";
+// static char urgfloatcolor[]              = "#db8fd9";
 
 #if RENAMED_SCRATCHPADS_PATCH
 static char scratchselfgcolor[]          = "#FFF7D4";
@@ -348,20 +351,21 @@ static char *colors[][ColCount] = {
 	#endif // BAR_FLEXWINTITLE_PATCH
 };
 
-#if BAR_POWERLINE_STATUS_PATCH
-static char *statuscolors[][ColCount] = {
-	/*                       fg                bg                border                float */
-	[SchemeNorm]         = { normfgcolor,      normbgcolor,      normbordercolor,      normfloatcolor },
-	[SchemeSel]          = { selfgcolor,       selbgcolor,       selbordercolor,       selfloatcolor },
-	[SchemeTitleNorm]    = { titlenormfgcolor, titlenormbgcolor, titlenormbordercolor, titlenormfloatcolor },
-	[SchemeTitleSel]     = { titleselfgcolor,  titleselbgcolor,  titleselbordercolor,  titleselfloatcolor },
-	[SchemeTagsNorm]     = { tagsnormfgcolor,  tagsnormbgcolor,  tagsnormbordercolor,  tagsnormfloatcolor },
-	[SchemeTagsSel]      = { tagsselfgcolor,   tagsselbgcolor,   tagsselbordercolor,   tagsselfloatcolor },
-	[SchemeHidNorm]      = { hidnormfgcolor,   hidnormbgcolor,   c000000,              c000000 },
-	[SchemeHidSel]       = { hidselfgcolor,    hidselbgcolor,    c000000,              c000000 },
-	[SchemeUrg]          = { urgfgcolor,       urgbgcolor,       urgbordercolor,       urgfloatcolor },
-};
-#endif // BAR_POWERLINE_STATUS_PATCH
+// TODO: Find a way to default to this if no theme is included
+// #if BAR_POWERLINE_STATUS_PATCH
+// static char *statuscolors[][ColCount] = {
+// 	/*                       fg                bg                border                float */
+// 	[SchemeNorm]         = { normfgcolor,      normbgcolor,      normbordercolor,      normfloatcolor },
+// 	[SchemeSel]          = { selfgcolor,       selbgcolor,       selbordercolor,       selfloatcolor },
+// 	[SchemeTitleNorm]    = { titlenormfgcolor, titlenormbgcolor, titlenormbordercolor, titlenormfloatcolor },
+// 	[SchemeTitleSel]     = { titleselfgcolor,  titleselbgcolor,  titleselbordercolor,  titleselfloatcolor },
+// 	[SchemeTagsNorm]     = { tagsnormfgcolor,  tagsnormbgcolor,  tagsnormbordercolor,  tagsnormfloatcolor },
+// 	[SchemeTagsSel]      = { tagsselfgcolor,   tagsselbgcolor,   tagsselbordercolor,   tagsselfloatcolor },
+// 	[SchemeHidNorm]      = { hidnormfgcolor,   hidnormbgcolor,   c000000,              c000000 },
+// 	[SchemeHidSel]       = { hidselfgcolor,    hidselbgcolor,    c000000,              c000000 },
+// 	[SchemeUrg]          = { urgfgcolor,       urgbgcolor,       urgbordercolor,       urgfloatcolor },
+// };
+// #endif // BAR_POWERLINE_STATUS_PATCH
 
 #if BAR_LAYOUTMENU_PATCH
 static const char *layoutmenu_cmd = "layoutmenu.sh";
